868af1560d5e10e1cc65cd80bd31cb2d922078ce
tolstovatatiana <tatiana.beto99@gmail.com>
2022-06-15 19:33:57 +0600

    I added changes

--- module.py
+++ module.py
@@ -1,3 +1,16 @@
+class Parent():
+    def myself(self):
+        print("I am a mom")
 
-class Human:
-	pass	
+class Child(Parent):
+    def play(self):
+        print("Hello, world")
+
+    def myself(self):
+        print("I am a baby")
+
+
+parent = Parent()
+parent.myself()
+child = Child()
+child.myself()
